192e065b2d71750921b7768dc5dcba5a38091996
Hosa <126433144+HiImHosa23@users.noreply.github.com>
2026-01-19 22:32:33 -0700

    editing main dart, adding login, signin, and textfield and textfield logic. (Previous repository broke. Started work at 8:00 pm new repo at 10:12)

---
+++ lib/textfield.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class TheTextField extends StatelessWidget {
+  final controller;
+  final String hintText;
+  final bool obscureText;
+
+  const TheTextField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 25.0),
+      child: TextField(
+        controller: controller,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey.shade900),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey.shade900),
+          ),
+          fillColor: Colors.white,
+          filled: true,
+          hintStyle: TextStyle(
+            color: Colors.grey.shade600,
+          ),
+        ),
+      ),
+    );
+  }
+}
