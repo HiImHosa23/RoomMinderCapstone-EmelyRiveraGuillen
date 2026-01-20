922c872d4ef0b7f05c8d947f30ba51ad2b67dfa0
Hosa <126433144+HiImHosa23@users.noreply.github.com>
2026-01-19 23:18:35 -0700

    Signin Page design complete (not styling) route to login and prints temporary

--- lib/signin.dart
+++ lib/signin.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:roommindercapstone/textfield.dart';
 
 class SignInPage extends StatefulWidget {
   const SignInPage({super.key});
@@ -8,8 +9,90 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPageState extends State<SignInPage> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController userController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(15.0),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.person_add,
+                  size: 100,
+                ),
+                Text(
+                  "RoomMinder Sign Up",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 20),
+                TheTextField(
+                  controller: nameController,
+                  hintText: 'Name',
+                  obscureText: false,
+                ),
+                SizedBox(height: 15),
+                TheTextField(
+                  controller: userController,
+                  hintText: 'Username',
+                  obscureText: false,
+                ),
+                SizedBox(height: 15),
+                TheTextField(
+                  controller: emailController,
+                  hintText: 'Email',
+                  obscureText: false,
+                ),
+                SizedBox(height: 15),
+                TheTextField(
+                  controller: passwordController,
+                  hintText: 'Password',
+                  obscureText: true,
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: (){
+                    print("Name: ${nameController.text}");
+                    print("Username: ${userController.text}");
+                    print("Email: ${emailController.text}");
+                    print("Password: ${passwordController.text}");
+                  },
+                  child: Text('Sign Up'),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Already a roommate? "),
+                    GestureDetector(
+                      onTap: (){
+                        Navigator.pop(context);
+                      },
+                      child: Text(
+                        'Sign In',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      )
+    );
   }
 }
