3149834425b72a56dd288c7e53b09a552d02658d
Hosa <126433144+HiImHosa23@users.noreply.github.com>
2026-01-21 23:16:12 -0700

    Added shared preferences. Added user class.

---
+++ lib/models/user.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+
+class User{
+  final String name;
+  final String username;
+  final String email;
+  final String password;
+
+  User({
+    required this.name,
+    required this.username,
+    required this.email,
+    required this.password
+  });
+
+//  create a map
+}
---
+++ lib/services/auth_service.dart
@@ -0,0 +1,6 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import '../models/user.dart';
+
+class AuthService{
+  //look into the logic more
+}
